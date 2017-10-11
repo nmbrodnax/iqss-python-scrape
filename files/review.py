110f13eafff6460d62768d9f1e56fb882e30752b
NaLette Brodnax <nmbrodnax@gmail.com>
2017-10-11 19:54:09 -0400

    revised content

--- files/review.py
+++ files/review.py
@@ -7,47 +7,31 @@
 # nbrodnax@iq.harvard.edu
 # www.nalettebrodnax.com
 
-print("Hello, world.")
 
-mystring = 'happy'
-print(mystring[0])
-print(mystring[2:4])
+datasets = ['airport', 'baseball', 'calcium', 'electricbill', 'freethrows']
+print(datasets[0])
+print(datasets[2:4])
+print(datasets[-2])
 
-mylist = ['Leia', 'Rey', 'Maz']
-print(mylist[-1])
+for name in datasets:
+    print(name + '.txt')
 
-mydict = {'name': 'Kylo', 'side': 'dark'}
-print(mydict['name'])
+data_info = {'filename': 'airport', 'title': 'US Airport Statistics',
+             'observations': 135}
+print(data_info['title'])
 
-name = 'Grace Hopper'
+import requests
+page = requests.get('http://ww2.amstat.org/publications/jse/jse_data_archive.htm')
+print(page)
 
-if len(name) < 20:
-    print('Yes')
-else:
-    print('No')
+def add_extensions(names):
+    """adds '.txt' to each string in a list
+    list of str -> list of str"""
+    filenames = []
+    for name in names:
+        filenames.append(name + '.txt')
+    return filenames
 
-i = 0
-for letter in name:
-    if letter in ['a', 'e', 'i', 'o', 'u']:
-        i = i + 1
-print(name + ' has ' + str(i) + ' vowels.')
+print(add_extensions(datasets))
 
-i = 0
-vowel_count = 0
-while i < len(name):
-    if name[i] in ['a', 'e', 'i', 'o', 'u']:
-        vowel_count = vowel_count + 1
-    i = i + 1
-print(name + ' has ' + str(vowel_count) + ' vowels.')
-
-my_string = 'aBcDe'
-print(my_string)
-
-print(my_string.lower())
-
-
-def say_hello(name_string):
-    print('Hello, ' + str(name_string) + '!')
-    return None
-
-say_hello('NaLette')
+      
